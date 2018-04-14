fab975036b6126cd74adabb56ae6a1fc2f62131f
dawneraq <dawneraq@gmail.com>
2018-04-13 20:17:05 -0400

    Modify streamgraph to display mortality data

---
+++ harvest_yearly_death_data.py
@@ -0,0 +1,53 @@
+"""
+Condense data for each cause of death into a two-column CSV: year and mortality rate, as an average of each state's mortality rate.
+
+The input file, death_data_states, is the original dataset, but only including rows for states' mortality rates--not those of counties.
+"""
+
+
+from os.path import isdir, join
+import csv
+import operator
+
+
+directory = 'data'
+input_filename = 'death_data_states.csv'
+output_filename = 'death_data_annual.csv'
+
+
+def main():
+  cause_year_values = {}
+  death_data_annual = []
+
+  # We want to iterate through the death data multiple times, as inefficient as that is. So we store the data in death_data_annual
+  with open(join(directory, input_filename), 'r') as input_file:
+    csv_reader = csv.reader(input_file)
+    for row in csv_reader:
+      death_data_annual.append(row)
+
+  # Average together mortality rates for all 50 states
+  for line_index, death_stat in enumerate(death_data_annual):
+    if line_index != 0:
+      adding_value = float(death_stat[5]) / 50
+      # Keys are (cause_of_death, year)
+      dict_key = (death_stat[3], int(death_stat[4]))
+
+      if dict_key in cause_year_values:
+        cause_year_values[dict_key] += adding_value
+      else:
+        cause_year_values[dict_key] = 0
+
+  # Write cleaned data to a new file
+  with open(join(directory, output_filename), 'w') as output_file:
+    csv_writer = csv.writer(output_file)
+    # Write header row
+    csv_writer.writerow(['year', 'cause_of_death', 'mortality_rate'])
+
+    # Ensure data is listed in order of year, then alphabetically by cause of disease
+    # for cause_year in sorted(cause_year_values, key=operator.itemgetter(0, 1)):
+    for cause_year in cause_year_values:
+      csv_writer.writerow([cause_year[1], cause_year[0], cause_year_values[cause_year]])
+
+
+if __name__ == '__main__':
+  main()
--- streamgraph.js
+++ streamgraph.js
@@ -1,38 +1,35 @@
-// http://bl.ocks.org/WillTurman/4631136
+// <!-- http://bl.ocks.org/WillTurman/4631136 -->
 
-chart("data.csv", "orange");
+chart('data/death_data_annual.csv');
 
 var datearray = [];
 var colorrange = [];
 
+function chart(csvpath) {
+  colorrange = [];
+  // We have 24 causes of death
+  let numColors = 24;
+  for (let i = 0; i < numColors; i++) {
+    let newColor = d3.interpolateRainbow(1.0 * i / numColors);
+    colorrange.push(newColor);
+  }
 
-function chart(csvpath, color) {
-  
-  if (color == "blue") {
-    colorrange = ["#045A8D", "#2B8CBE", "#74A9CF", "#A6BDDB", "#D0D1E6", "#F1EEF6"];
-  }
-  else if (color == "pink") {
-    colorrange = ["#980043", "#DD1C77", "#DF65B0", "#C994C7", "#D4B9DA", "#F1EEF6"];
-  }
-  else if (color == "orange") {
-    colorrange = ["#B30000", "#E34A33", "#FC8D59", "#FDBB84", "#FDD49E", "#FEF0D9"];
-  }
   strokecolor = colorrange[0];
   
-  var format = d3.time.format("%m/%d/%y");
+  var format = d3.time.format('%Y');
   
   var margin = {top: 20, right: 40, bottom: 30, left: 30};
   var width = document.body.clientWidth - margin.left - margin.right;
   var height = 400 - margin.top - margin.bottom;
   
-  var tooltip = d3.select("body")
-  .append("div")
-  .attr("class", "remove")
-  .style("position", "absolute")
-  .style("z-index", "20")
-  .style("visibility", "hidden")
-  .style("top", "30px")
-  .style("left", "55px");
+  var tooltip = d3.select('body')
+  .append('div')
+  .attr('class', 'remove')
+  .style('position', 'absolute')
+  .style('z-index', '20')
+  .style('visibility', 'hidden')
+  .style('top', '30px')
+  .style('left', '55px');
   
   var x = d3.time.scale()
   .range([0, width]);
@@ -45,129 +42,129 @@ function chart(csvpath, color) {
   
   var xAxis = d3.svg.axis()
   .scale(x)
-  .orient("bottom")
-  .ticks(d3.time.weeks);
+  .orient('bottom')
+  .ticks(d3.time.years);
   
   var yAxis = d3.svg.axis()
   .scale(y);
   
   var yAxisr = d3.svg.axis()
   .scale(y);
-  
+
   var stack = d3.layout.stack()
-  .offset("silhouette")
+  .offset('silhouette')
   .values(function(d) { return d.values; })
-  .x(function(d) { return d.date; })
-  .y(function(d) { return d.value; });
-  
+  .x(function(d) { return d.year; })
+  .y(function(d) { return d.mortality_rate; });
+
   var nest = d3.nest()
-  .key(function(d) { return d.key; });
+  .key(function(d) { return d.cause_of_death; });
   
   var area = d3.svg.area()
-  .interpolate("cardinal")
-  .x(function(d) { return x(d.date); })
+  .interpolate('cardinal')
+  .x(function(d) { return x(d.year); })
   .y0(function(d) { return y(d.y0); })
   .y1(function(d) { return y(d.y0 + d.y); });
   
-  var svg = d3.select(".chart").append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var svg = d3.select('.chart').append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
   
   var graph = d3.csv(csvpath, function(data) {
     data.forEach(function(d) {
-      d.date = format.parse(d.date);
-      d.value = +d.value;
+      d.year = format.parse(d.year);
+      d.mortality_rate = +d.mortality_rate;
     });
     
     var layers = stack(nest.entries(data));
     
-    x.domain(d3.extent(data, function(d) { return d.date; }));
+    x.domain(d3.extent(data, function(d) { return d.year; }));
     y.domain([0, d3.max(data, function(d) { return d.y0 + d.y; })]);
     
-    svg.selectAll(".layer")
+    svg.selectAll('.layer')
     .data(layers)
-    .enter().append("path")
-    .attr("class", "layer")
-    .attr("d", function(d) { return area(d.values); })
-    .style("fill", function(d, i) { return z(i); });
+    .enter().append('path')
+    .attr('class', 'layer')
+    .attr('d', function(d) { return area(d.values); })
+    .style('fill', function(d, i) { return z(i); });
     
     
-    svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
+    svg.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + height + ')')
     .call(xAxis);
     
-    svg.append("g")
-    .attr("class", "y axis")
-    .attr("transform", "translate(" + width + ", 0)")
-    .call(yAxis.orient("right"));
+    svg.append('g')
+    .attr('class', 'y axis')
+    .attr('transform', 'translate(' + width + ', 0)')
+    .call(yAxis.orient('right'));
     
-    svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis.orient("left"));
+    svg.append('g')
+    .attr('class', 'y axis')
+    .call(yAxis.orient('left'));
     
-    svg.selectAll(".layer")
-    .attr("opacity", 1)
-    .on("mouseover", function(d, i) {
-      svg.selectAll(".layer").transition()
+    svg.selectAll('.layer')
+    .attr('opacity', 1)
+    .on('mouseover', function(d, i) {
+      svg.selectAll('.layer').transition()
       .duration(250)
-      .attr("opacity", function(d, j) {
+      .attr('opacity', function(d, j) {
         return j != i ? 0.6 : 1;
       })})
       
-    .on("mousemove", function(d, i) {
-      mousex = d3.mouse(this);
-      mousex = mousex[0];
-      var invertedx = x.invert(mousex);
-      invertedx = invertedx.getMonth() + invertedx.getDate();
-      var selected = (d.values);
-      for (var k = 0; k < selected.length; k++) {
-        datearray[k] = selected[k].date
-        datearray[k] = datearray[k].getMonth() + datearray[k].getDate();
-      }
+      .on('mousemove', function(d, i) {
+        mousex = d3.mouse(this);
+        mousex = mousex[0];
+        var invertedx = x.invert(mousex);
+        invertedx = invertedx.getFullYear();
+        var selected = (d.values);
+        for (var k = 0; k < selected.length; k++) {
+          datearray[k] = selected[k].year;
+          datearray[k] = datearray[k].getFullYear();
+        }
+        
+        mousedate = datearray.indexOf(invertedx);
+        pro = d.values[mousedate].mortality_rate;
+        
+        d3.select(this)
+        .classed('hover', true)
+        .attr('stroke', strokecolor)
+        .attr('stroke-width', '0.5px'), 
+        tooltip.html( '<p>' + d.key + '<br>' + pro + '</p>' ).style('visibility', 'visible');
+        
+      })
+      .on('mouseout', function(d, i) {
+        svg.selectAll('.layer')
+        .transition()
+        .duration(250)
+        .attr('opacity', '1');
+        d3.select(this)
+        .classed('hover', false)
+        .attr('stroke-width', '0px'), tooltip.html( '<p>' + d.cause_of_death + '<br>' + pro + '</p>' ).style('visibility', 'hidden');
+      });
       
-      mousedate = datearray.indexOf(invertedx);
-      pro = d.values[mousedate].value;
+      var vertical = d3.select('.chart')
+        .append('div')
+        .attr('class', 'remove')
+        .style('position', 'absolute')
+        .style('z-index', '19')
+        .style('width', '1px')
+        .style('height', '380px')
+        .style('top', '10px')
+        .style('bottom', '30px')
+        .style('left', '0px')
+        .style('background', '#fff');
       
-      d3.select(this)
-      .classed("hover", true)
-      .attr("stroke", strokecolor)
-      .attr("stroke-width", "0.5px"), 
-      tooltip.html( "<p>" + d.key + "<br>" + pro + "</p>" ).style("visibility", "visible");
-      
-    })
-    .on("mouseout", function(d, i) {
-      svg.selectAll(".layer")
-      .transition()
-      .duration(250)
-      .attr("opacity", "1");
-      d3.select(this)
-      .classed("hover", false)
-      .attr("stroke-width", "0px"), tooltip.html( "<p>" + d.key + "<br>" + pro + "</p>" ).style("visibility", "hidden");
-    })
-    
-    var vertical = d3.select(".chart")
-    .append("div")
-    .attr("class", "remove")
-    .style("position", "absolute")
-    .style("z-index", "19")
-    .style("width", "1px")
-    .style("height", "380px")
-    .style("top", "10px")
-    .style("bottom", "30px")
-    .style("left", "0px")
-    .style("background", "#fff");
-    
-    d3.select(".chart")
-    .on("mousemove", function(){  
-      mousex = d3.mouse(this);
-      mousex = mousex[0] + 5;
-      vertical.style("left", mousex + "px" )})
-      .on("mouseover", function(){  
+      d3.select('.chart')
+      .on('mousemove', function(){  
         mousex = d3.mouse(this);
         mousex = mousex[0] + 5;
-        vertical.style("left", mousex + "px")});
-      });
+        vertical.style('left', mousex + 'px' )})
+        .on('mouseover', function(){  
+          mousex = d3.mouse(this);
+          mousex = mousex[0] + 5;
+          vertical.style('left', mousex + 'px')});
+        });
 }
